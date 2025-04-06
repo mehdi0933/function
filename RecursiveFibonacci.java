6fffa0414b871ae147bc178466d90944b1194678
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-04-06 02:36:48 +0200

    Add files via upload

--- RecursiveFibonacci.java
+++ RecursiveFibonacci.java
@@ -2,18 +2,16 @@ package javacore.chapter04.function.exercise;
 
 public class RecursiveFibonacci {
     public static void main(String[] args) {
-        RecursiveFibonacci(0,1,0, 50);
-        System.out.println();
+         RecursiveFibonacci(0 , 1);
     }
-    public static int RecursiveFibonacci(int value1, int value2, int resulta, int valueMax ) {
-        if(value1 <= valueMax) {
-            System.out.println(resulta);
-            resulta = value2 + value1;
-            value1 = value2;
-            value2 = resulta;
-            RecursiveFibonacci(value1, value2, resulta);
+
+    public static int RecursiveFibonacci(int value1 , int value2) {
+        if (value1 <= 50) {
+            System.out.println(value1);
+            RecursiveFibonacci(value2, value1 + value2);
             return value1;
         }
         return 1;
+
     }
 }
